0efe96c38ff02d022c4c880fe12b014e64375cb1
jmattgiroux <jmattgiroux@gmail.com>
2023-02-27 14:52:12 -0600

    Used what I learned from previous step to move the top-menu to a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CourseCardComponent } from './course-card/course-card.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseCardComponent
+    CourseCardComponent,
+    TopMenuComponent
   ],
   imports: [
     BrowserModule,
