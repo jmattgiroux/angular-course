69d391da105e272890e868f78b7c2953bd9c8202
jmattgiroux <jmattgiroux@gmail.com>
2023-03-08 12:47:19 -0600

    Finished Angular Component @Output - Custom Events and Event Emitters

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,14 +1,20 @@
-import { Component, Input } from '@angular/core';
-import { Course } from '../model/course';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Course } from "../model/course";
 
 @Component({
-  selector: 'course-card',
-  templateUrl: './course-card.component.html',
-  styleUrls: ['./course-card.component.css']
+  selector: "course-card",
+  templateUrl: "./course-card.component.html",
+  styleUrls: ["./course-card.component.css"],
 })
 export class CourseCardComponent {
-
   @Input()
   course: Course;
 
+  @Output("courseSelected")
+  courseEventEmitter = new EventEmitter<Course>();
+
+  onCourseViewed() {
+    console.log("card component - button clicked...");
+    this.courseEventEmitter.emit(this.course);
+  }
 }
