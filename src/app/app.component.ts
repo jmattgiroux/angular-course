4ee66adfe2b01adaf2e442564050e5ba8ab50cce
jmattgiroux <jmattgiroux@gmail.com>
2023-03-22 22:50:11 -0500

    Commented out code for presentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,24 @@ import { Course } from "./model/course";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  courses = COURSES.filter((course: Course) => course !== undefined);
+  /**
+   * Start of JsonOutputFeature
+   */
+  // courses = COURSES.filter((course: Course) => course !== undefined);
+  //
+  // startDate = new Date(2000, 0, 1);
+  //
+  // title = this.courses[0].description;
+  //
+  // price = 9.99;
+  //
+  // rate = 0.54;
+  //
+  // course = this.courses[0];
 
-  startDate = new Date(2000, 0, 1);
-
-  title = this.courses[0].description;
-
-  price = 9.99;
-
-  rate = 0.54;
-
-  course = this.courses[0];
+  /**
+   * End of JsonOutputFeature
+   */
 
   onCourseSelected(course: Course) {
     console.log("App Component - click event bubbled...", course);
