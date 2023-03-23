b0e7de4f8fb79861978494ce5416e42363c030fc
jmattgiroux <jmattgiroux@gmail.com>
2023-03-22 22:57:15 -0500

    Uncommented out code for presentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,17 @@ export class AppComponent {
   /**
    * Start of JsonOutputFeature
    */
-  // courses = COURSES.filter((course: Course) => course !== undefined);
-  //
-  // startDate = new Date(2000, 0, 1);
-  //
-  // title = this.courses[0].description;
-  //
-  // price = 9.99;
-  //
-  // rate = 0.54;
-  //
-  // course = this.courses[0];
+  courses = COURSES.filter((course: Course) => course !== undefined);
+
+  startDate = new Date(2000, 0, 1);
+
+  title = this.courses[0].description;
+
+  price = 9.99;
+
+  rate = 0.54;
+
+  course = this.courses[0];
 
   /**
    * End of JsonOutputFeature
